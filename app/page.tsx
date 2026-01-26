868d3126253f70ebb6b68c0346cb9d086369fe9c
Flaviane Lima <153138949+flaviane-lima@users.noreply.github.com>
2026-01-26 23:38:04 +0000

    import do Component Header, que contém o logotipo do projeto

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+'use cliente' 
+import Header from "./components/Header"
+
 export default function Page() {
-  return <h1>Olá, Seja Bem vindo! Me chamo Flaviane</h1>
+  return (
+    <div>
+      <Header />
+      <section>
+      <h1>Olá, Seja Bem vindo! Me chamo Flaviane</h1>
+      </section>
+    </div>
+    
+  )
 }
