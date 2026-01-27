50b81b21e54752ce5b99a33a13f93d90d0b5aae3
Flaviane Lima <153138949+flaviane-lima@users.noreply.github.com>
2026-01-27 01:12:38 +0000

    estilização e acrescentei um paragrafo com uma mensagem junto ao header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,52 @@
 'use client'
 
+
+import styled from "styled-components";
+
+const Logo = styled.header`
+ background-color: #1E3A8A;
+ color: white;
+ text-align: center;
+ padding: 2rem 1rem;
+
+ h1 {
+   font-size: 1.875rem;
+   font-weight: bold;
+   margin: 0;
+}
+   p {
+   font-size: 1rem;
+   margin-top: 0.5rem;
+   color: #E0E7FF; /* azul claro para contraste */
+   max-width: 600px; /* limita largura para não ficar muito esticado */
+
+   }
+
+   @media (max-width: 600px) {
+     h1 {
+      font-size: 1.5rem;
+    }
+    p {
+      font-size: 0.875rem;
+    }
+   }
+
+   @media (max-width: 400px) {
+      h1 {
+      font-size: 1.25rem;
+    }
+    p {
+      font-size: 0.75rem;
+    }
+ }
+`
+
 export default function Header() {
     return (
-    <h1>Flaviane de Lima</h1>
+    <Logo>
+        <h1>Flaviane de Lima</h1>
+        <p>A tecnologia é uma ponte entre ideias e impactos</p>
+    </Logo>
+    
     )
 }
