7598d30587397915c55892a443013a1154930f28
Flaviane Lima <153138949+flaviane-lima@users.noreply.github.com>
2026-01-29 00:51:51 +0000

    adicionar array de projetos com dados do My Finance

---
+++ app/components/Projects.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { Project } from "./types";
+import Card from "./Card";
+
+// vou criar um array
+const projects: Project[] = [
+    {
+        title: "My Finance",
+        image: "/images/myFinance.jpeg",
+        description: "Gestão financeira pessoal simples e organizada.",
+        technologies: ["Tailwind", "TypeScript", "Next.js", "Prisma ORM", "Docker"],
+        status: "Em andamento",
+        github: "https://github.com/flaviane-lima/myfinance"
+    }
+];
+
+export default function () {
+    return (
+        <div>
+            {projects.map((project) => (
+                <Card 
+                 key={project.title}
+                 title={project.title}
+                 image={project.image}
+                 description={project.description}
+                 technologies={project.technologies}
+                 status={project.status}
+                 github={project.github}
+                />
+            ))}
+        </div>
+    )
+}
