2225143bd8569878616208a7eb6a7216f2ed3218
Flaviane Lima <153138949+flaviane-lima@users.noreply.github.com>
2026-01-29 00:47:21 +0000

    add Project e CardProps interface to types.ts

---
+++ app/components/types.ts
@@ -0,0 +1,17 @@
+export interface CardProps  {
+    title: string;
+    image: string;
+    description: string;
+    technologies: string[];
+    status: string;
+    github: string;
+}
+
+export interface Project {
+    title: string;
+    image: string;
+    description: string;
+    technologies: string[];
+    status: string;
+    github: string;
+}
