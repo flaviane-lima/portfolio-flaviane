6e874ce5f0b3b446d1c24df499c173a729bb5550
Flaviane Lima <153138949+flaviane-lima@users.noreply.github.com>
2026-01-29 00:51:17 +0000

    importar componente Projects para renderizar os cartões

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 import Header from "./components/Header"
 import PageWrapper from "./components/PageWrapper"
 import AboutMe from "./components/AboutMe"
+import Projects from "./components/Projects"
 
 export default function Page() {
   return (
     <PageWrapper>
       <Header />
         <AboutMe />
+        <Projects />
     </PageWrapper>
       
     
