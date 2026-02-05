706ee4ca160ea0d011b796a0636c4d380da13a57
Flaviane Lima <153138949+flaviane-lima@users.noreply.github.com>
2026-02-05 01:01:21 +0000

    começo da estilização da image e mudança necessária na estrutura do código

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,26 +2,34 @@
 
 import Image from "next/image";
 import { CardProps } from "./types";
+import styled from "styled-components";
+
+const StyledImage = styled(Image)`
+    object-fit: cover;
+    border-radius: 8px;  
+`
 
 //recebe os dados via props
 export default function Card({ title, image, description, technologies, status, github }: CardProps) {
   return (
+    <section>
+      <h2>Projetos</h2>
     <div>
-      <Image
+      <h3>{title}</h3>
+      <StyledImage
         src={image}
         alt={title}
-        fill
         quality={100}
-        className="card_img"
-        style={{ objectFit: "cover" }}
+        width={400}
+        height={250}
       />
       <div>
-        <h2>{title}</h2>
         <p>{description}</p>
         <p>Tecnologias: {technologies}</p>
         <p>Status: {status}</p>
-        <a href={github} target="_blank" className="button">GitHub</a>
+        <a href={github} target="_blank" className="button">GitHub:</a>
       </div>
     </div>
+    </section>
   );
 }
