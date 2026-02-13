dc39d7350edcf12f07ff1c7280764edcafd50e95
Flaviane Lima <153138949+flaviane-lima@users.noreply.github.com>
2026-02-13 23:11:50 +0000

    remover Footer da Page e centralizar controle no PageWrapper

--- app/components/PageWrapper.tsx
+++ app/components/PageWrapper.tsx
@@ -2,15 +2,28 @@
 
 import React from "react"
 import styled from "styled-components"
+import Footer from "./Footer"
 
 const StyledWrapper = styled.div`
 background-color:  #F3F4F6; /* cinza claro */
 min-height: 100vh;
 display: flex;
 flex-direction: column;
+padding-bottom: 80px; /* altura do footer para não encobrir conteúdo */
 
 `
 
+const Content = styled.main`
+  flex: 1;
+`
+
 export default function PageWrapper({ children }: { children: React.ReactNode}) {
-    return <StyledWrapper>{ children }</StyledWrapper>
+    return (
+    <StyledWrapper>
+        <Content>
+            { children }
+        </Content>
+        <Footer />
+    </StyledWrapper>
+    )
 }
